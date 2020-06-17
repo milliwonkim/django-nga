61ffe94b5a0a82558ee5fc6fde899ca36f2e9e93
milliwonkim <rldnjs9347@gmail.com>
2020-06-18 03:24:26 +0900

    200618

--- NGA/settings.py
+++ NGA/settings.py
@@ -131,6 +131,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'account', 'blogapp', 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SITE_ID = 1
